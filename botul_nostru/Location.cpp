444aa1a0c49dfa7bd41474d4f0c01b55076eaf9b
gg.kaspersky <gg.kaspersky@gmail.com>
2012-03-08 21:51:45 +0200

    Added a round2 script.

--- botul_nostru/Location.cpp
+++ botul_nostru/Location.cpp
@@ -13,36 +13,26 @@
  */
 
 #include "Location.h"
-/** Operator for sort utility */
+
 bool Location::operator == (Location param)
 {
     return col == param.col && row == param.row;
 }
 
-/** Structure for representing a location on the map. */
 Location Location::move(int dir) 
 {
-    Location returnValue(row + ROW_DIRECTION[dir], col + COLUMN_DIRECTION[dir]);
-    
-    if (returnValue.row < 0) 
-    {
-        returnValue.row += gparam::mapRows;
-    } 
-    else if (returnValue.row == gparam::mapRows)
-    {
-        returnValue.row = 0;
-    }
-
-    if (returnValue.col < 0)
-    {
-        returnValue.col += gparam::mapColumns;
-    } 
-    else if (returnValue.col == gparam::mapColumns)
-    {
-        returnValue.col = 0;
-    }
-
-    return returnValue;
+    Location rez(row + ROW_DIRECTION[dir], col + COLUMN_DIRECTION[dir]);
+    if (rez.row < 0)
+        rez.row += gparam::mapRows;
+    else
+        if (rez.row == gparam::mapRows)
+            rez.row = 0;
+    if (rez.col < 0)
+        rez.col += gparam::mapColumns;
+    else
+        if (rez.col == gparam::mapColumns)
+            rez.col = 0;
+    return rez;
 }
 
 Location Location::move(int x,int y)
--- botul_nostru/Location.h
+++ botul_nostru/Location.h
@@ -1,15 +1,20 @@
 #include "global.h"
 
-struct Location
+class Location
 {
-    int row;
-    int col;
-    int f;
-    int g;
-    int dir;
-    Location() : row(0), col(0) { }
+
+public:
+	int row;
+	int col;
+	int f;
+	int g;
+	int dir;
+    
+	Location() : row(0), col(0) { }
     Location(int row, int col) : row(row), col(col) { }
-    bool operator == (Location);
+    
+	bool operator == (Location);
+
     Location move(int dir);
 
     Location move(int x,int y);
