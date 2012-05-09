1dc7d2ae5b43885f6f2403ed1c81b898b6657a12
Vladimir Cernov <gg.kaspersky@gmail.com>
2012-05-10 01:11:15 +0300

    Edited round3 script, added tags to ignore list.

--- round3_64.sh
+++ round3_64.sh
@@ -7,8 +7,8 @@ contrabot='./bot3_64'
 cp botul_nostru/bin/MyBot .
 
 ./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/cell_maze/cell_maze_p02_10.map "$contrabot" "$bot"
-#./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/cell_maze/cell_maze_p02_15.map "$contrabot" "$bot"
-#./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/maze/maze_p02_14.map "$contrabot" "$bot"
-#./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/random_walk/random_walk_p02_10.map "$contrabot" "$bot"
+./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/cell_maze/cell_maze_p02_15.map "$contrabot" "$bot"
+./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/maze/maze_p02_14.map "$contrabot" "$bot"
+./playgame.py -v -t 1000 --engine_seed 42 --player_seed 0 -l game_logs -m maps/random_walk/random_walk_p02_10.map "$contrabot" "$bot"
 
 exit 0
